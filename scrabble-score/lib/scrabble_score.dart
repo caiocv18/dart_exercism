a3777550a6a22e849fcac52380e6cc090465acc4
Caio Vinicius <caioviniciuscv18@gmail.com>
2023-01-10 18:14:32 -0300

    Adding Scrabble Score exercise solution

--- scrabble-score/lib/scrabble_score.dart
+++ scrabble-score/lib/scrabble_score.dart
@@ -1,1 +1,15 @@
-// Put your code here
+import 'package:scrabble_score/scrabble_maping.dart';
+
+int score(String word){
+  ScrabbleMapping map = new ScrabbleMapping();
+  List letters = word.split("");
+  int sum = 0;
+  for(var i in letters){
+    for(var l in map.letterPoints.keys){
+      if(i.toString().toUpperCase() == l){
+        sum += map.letterPoints[l]!;
+      }
+    }
+  }
+  return sum;
+}
